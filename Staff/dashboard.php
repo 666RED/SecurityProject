5b4ef49c532760f4408d6ef2b527fd8dc27c243b
SEE HONG CHEN <hongchensee8@gmail.com>
2024-05-23 03:41:32 +0800

    add basic files

--- Staff/dashboard.php
+++ Staff/dashboard.php
@@ -1,3 +1,12 @@
+<?php 
+  session_start();
+
+  if(!isset($_SESSION['admin'])) {
+    echo 'Please login first <br>';
+    echo 'Click <a href="index.php">here<a/> to login';
+  }else {
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,10 +14,97 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dashboard</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="./styles/dashboard.css">
+  <link rel="stylesheet" href="./styles/style.css">
+  <link rel="stylesheet" href="./styles/sidebar.css">
 </head>
 
 <body>
+  <?php 
+    // function getTotal($name){
 
+    //   include 'db.php';
+
+    //   $query = "SELECT COUNT(*) AS total_count FROM $name";
+
+    //   $result = mysqli_query($conn, $query);
+
+    //   if(mysqli_num_rows($result) > 0) {
+    //     while($row = mysqli_fetch_assoc($result)) {
+    //       return $row['total_count'];
+    //     }
+    //   }
+    // }
+
+    // function getTotalContact(){
+
+    //   include 'db.php';
+
+    //   $query = "SELECT COUNT(*) AS total_count FROM contact_us WHERE respond != 1";
+
+    //   $result = mysqli_query($conn, $query);
+
+    //   if(mysqli_num_rows($result) > 0) {
+    //     while($row = mysqli_fetch_assoc($result)) {
+    //       return $row['total_count'];
+    //     }
+    //   }
+    // }
+
+    // $numOfProduct = getTotal('product');
+    // $numOfEvent = getTotal('event');
+    // $numOfContact = getTotalContact();
+  ?>
+  <div class="container-fluid">
+    <!-- sidebar -->
+    <div class="bg-dark text-white sidebar">
+      <a href="dashboard.php" class="btn nav-btn text-white d-block text-start selected">Dashboard</a>
+      <a href="course.php" class="btn nav-btn text-white d-block text-start">Course</a>
+      <a href="lecturer.php" class="btn nav-btn text-white d-block text-start">Lecturer</a>
+      <a href="student.php" class="btn nav-btn text-white d-block text-start">Student</a>
+      <a href="clearId.php"
+        class="btn nav-btn text-white d-block text-start position-absolute w-100 bottom-0">Logout</a>
+    </div>
+
+    <div class="main-content">
+      <h1 class="title">Dashboard</h1>
+      <!-- card -->
+      <div class="my-5 card-container">
+        <div class="row">
+          <div class="col-lg-4 col-sm-12">
+            <div class="card mx-auto my-3 card-1" onclick="window.location.href = 'Course.php'">
+              <div class="card-body">
+                <h5 class="card-title">Total Course</h5>
+                <!-- <p class="card-text"><?php echo $numOfProduct;?></p> -->
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-sm-12">
+            <div class="card mx-auto my-3 card-2" onclick="window.location.href = 'lecturer.php'">
+              <div class="card-body">
+                <h5 class="card-title">Total Lecturer</h5>
+                <!-- <p class="card-text"><?php echo $numOfEvent?></p> -->
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-sm1-12">
+            <div class="card mx-auto my-3 card-3" onclick="window.location.href = 'student.php'">
+              <div class="card-body">
+                <h5 class="card-title">Total Student</h5>
+                <!-- <p class="card-text"><?php echo $numOfContact;?></p> -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </body>
 
 </html>
+
+<?php 
+  } 
+?>
--- Staff/index.php
+++ Staff/index.php
@@ -1,3 +1,12 @@
+<?php 
+  session_start();
+
+  if(isset($_SESSION['errorMessage'])) {
+    $errorMessage = $_SESSION['errorMessage'];
+    unset($_SESSION['errorMessage']);
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,14 +16,76 @@
   <title>Login</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+    integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <style>
+  body,
+  html {
+    height: 100%;
+    margin: 0;
+  }
+
+  .icon {
+    position: absolute;
+    cursor: pointer;
+    right: 8px;
+    top: 10px;
+    font-size: 18px;
+  }
+
+  .icon:hover {
+    color: blue;
+  }
+  </style>
 </head>
 
 <body>
-  <!-- LOGO AND SIGN UP BUTTON -->
-  <header class="d-flex justify-content-between align-items-center">
-    <h1>LOGO</h1>
-    <button class="btn btn-primary">Sign up</button>
-  </header>
+  <?php
+    if(!empty($errorMessage)) {
+      echo 
+        '<div class="alert alert-danger" role="alert">' . $errorMessage . '</div>';
+    }
+  ?>
+  <div class="d-flex justify-content-center align-items-center h-100">
+    <div>
+      <h3 class="text-center">Welcome back!</h3>
+      <p class="text-center">Login to your account :)</p>
+      <form style="min-width: 400px" action="authentication/verifyLogin.php" method="POST">
+        <!-- USER ID -->
+        <label for="id-input">User ID</label>
+        <input type="text" class="form-control" id="id-input" name="user-id" placeholder="Enter user ID">
+        <!-- PASSWORD & ICONS -->
+        <label for="password-input" class="mt-3">Password</label>
+        <div class="position-relative">
+          <i class="fa-solid fa-eye icon" id="open-eye" onclick="handleViewPassword(1)"></i>
+          <i class="fa-solid fa-eye-slash icon d-none" id="close-eye" onclick="handleViewPassword(0)"></i>
+          <input type="password" class="form-control mt-2" id="password-input" placeholder="Enter password"
+            name="password">
+        </div>
+        <!-- LOGIN BUTTON -->
+        <div class="text-center">
+          <button class="btn btn-primary w-50  mt-5" name="login">LOGIN</button>
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <script>
+  const handleViewPassword = (value) => {
+    if (value === 1) {
+      document.getElementById("open-eye").classList.add("d-none")
+      document.getElementById("close-eye").classList.remove("d-none")
+      document.getElementById("close-eye").classList.add("d-block")
+      document.getElementById("password-input").type = "text"
+    } else {
+      document.getElementById("open-eye").classList.remove("d-none")
+      document.getElementById("open-eye").classList.add("d-block")
+      document.getElementById("close-eye").classList.add("d-none")
+      document.getElementById("password-input").type = "password"
+    }
+  }
+  </script>
 </body>
 
 </html>
