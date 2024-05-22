354aecc4346f65634bbac1637fdd3e5306548dae
SEE HONG CHEN <hongchensee8@gmail.com>
2024-05-22 23:54:55 +0800

    first commit

---
+++ Staff/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+  <!-- LOGO AND SIGN UP BUTTON -->
+  <header class="d-flex justify-content-between align-items-center">
+    <h1>LOGO</h1>
+    <button class="btn btn-primary">Sign up</button>
+  </header>
+</body>
+
+</html>
